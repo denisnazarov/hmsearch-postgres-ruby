f39e09311460ad4d2999582dbbdbcdecef0dd200
Kris Selden <kris.selden@gmail.com>
2014-12-14 10:20:40 -0800

    fix issue hopefully with g++

--- ext/hmsearch/extconf.rb
+++ ext/hmsearch/extconf.rb
@@ -5,4 +5,14 @@ dir_config('pqxx')
 
 have_library('pqxx')
 
+with_cppflags('--std=c++11') do
+	try_cpp(<<-CPP)
+		#include <string>
+		int main() {
+			std::to_string(0);
+			return 0;
+		}
+	CPP
+end
+
 create_makefile("hmsearch/postgres_ext")
